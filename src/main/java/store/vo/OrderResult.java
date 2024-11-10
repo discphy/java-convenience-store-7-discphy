3747e3af999688013390e6c8042f2d53b1ed8bca
10 <dinophy@nate.com>
2024-11-10 19:52:53 +0900

    feat: VO 클래스 정의

---
+++ src/main/java/store/vo/OrderResult.java
@@ -0,0 +1,49 @@
+package store.vo;
+
+import store.entity.ProductInfo;
+import store.entity.ProductStock;
+
+public class OrderResult {
+
+    private final ProductInfo productInfo;
+    private final OrderResultQuantity orderResultQuantity;
+    private final ProductStock updateStock;
+
+    public OrderResult(ProductInfo productInfo, OrderResultQuantity orderResultQuantity, ProductStock updateStock) {
+        this.productInfo = productInfo;
+        this.orderResultQuantity = orderResultQuantity;
+        this.updateStock = updateStock;
+    }
+
+    public static OrderResult of(ProductInfo productInfo, OrderResultQuantity orderResultQuantity, ProductStock updateStock) {
+        return new OrderResult(productInfo, orderResultQuantity, updateStock);
+    }
+
+    public ProductInfo getProductInfo() {
+        return productInfo;
+    }
+
+    public OrderResultQuantity getOrderResultQuantity() {
+        return orderResultQuantity;
+    }
+
+    public ProductStock getUpdateStock() {
+        return updateStock;
+    }
+
+    public long getTotalPrice() {
+        return productInfo.getPrice() * orderResultQuantity.getTotalQuantity();
+    }
+
+    public long getPromotionDiscountPrice() {
+        return productInfo.getPrice() * orderResultQuantity.getFreeQuantity();
+    }
+
+    public long getMembershipDiscountPrice() {
+        if (orderResultQuantity.getFreeQuantity() > 0) {
+            return 0;
+        }
+
+        return productInfo.getPrice() * orderResultQuantity.getTotalQuantity();
+    }
+}
